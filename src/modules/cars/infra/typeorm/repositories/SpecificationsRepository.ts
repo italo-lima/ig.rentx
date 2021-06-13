b07cf69a95dd0790fd5d59c89e4b11a96184b932
Ítalo Lima <italojonas@hotmail.com>
2021-06-13 20:27:12 -0300

    added jest

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,6 @@
-import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
-import { Specification } from "../../entities/Specification"
 import { getRepository, Repository } from "typeorm";
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
+import { ICreateSpecificationDTO, ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
 
