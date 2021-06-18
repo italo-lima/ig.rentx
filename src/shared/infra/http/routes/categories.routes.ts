c6cfbcb3cf96c6aa272a3a48c575b59dc09cc5e9
Ítalo Lima <italojonas@hotmail.com>
2021-06-18 08:09:10 -0300

    created middleware ensureAdmin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 
 import multer from "multer"
 
-import {CreateCategoryController} from "@modules/cars/useCases/createCategory/CreatecategoryController";
+import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreatecategoryController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategotyController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
+
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 
 const categoriesRoutes = Router();
 
@@ -16,10 +19,15 @@ const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
 const listCategoriesController = new ListCategoriesController()
 
-categoriesRoutes.post("/", createCategoryController.handle)
+categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle)
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
-categoriesRoutes.post('/import', upload.single("file"), importCategoryController.handle)
+categoriesRoutes.post(
+  '/import',
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.single("file"),
+  importCategoryController.handle)
 
 export { categoriesRoutes };
