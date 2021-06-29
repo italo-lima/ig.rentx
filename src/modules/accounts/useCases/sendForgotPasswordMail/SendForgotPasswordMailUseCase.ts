ac1b0f1444187d394b603e7eedbb63b7d6583ded
Ítalo Lima <italojonas@hotmail.com>
2021-06-29 15:53:25 -0300

    template forgot email

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,10 +1,12 @@
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
+import { resolve } from "path"
+
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject, injectable } from "tsyringe";
-import { v4 as uuidV4 } from "uuid";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -23,6 +25,8 @@ class SendForgotPasswordMailUseCase {
   async execute(email: string) {
     const user = await this.usersRepository.findByEmail(email)
 
+    const templatePath = resolve(__dirname, "..", "..", "views", "emails", "forgotPassword.hbs")
+
     if (!user) {
       throw new AppError("User does not exists!")
     }
@@ -37,10 +41,16 @@ class SendForgotPasswordMailUseCase {
       expires_date,
     })
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`
+    }
+
     await this.MailProvider.sendMail(
       email,
       "Recuperação de senha",
-      `O link para o reset é ${token}`
+      variables,
+      templatePath
     )
   }
 }
