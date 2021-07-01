9c1c7ef8d56f394d95e5f5fb7c7233c123b58f92
Ítalo Lima <italojonas@hotmail.com>
2021-07-01 20:18:07 -0300

    adjuste refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string
+}
+
 @injectable()
 class RefreshTokenUseCase {
 
@@ -20,7 +25,7 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) { }
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
 
     const { sub, email } = verify(token, auth.secret_refresh_token) as IPayload
 
@@ -48,7 +53,15 @@ class RefreshTokenUseCase {
       refresh_token
     })
 
-    return refresh_token
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token
+    })
+
+    return {
+      token: newToken,
+      refresh_token
+    }
   }
 }
 
