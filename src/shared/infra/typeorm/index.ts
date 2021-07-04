abfaa1dc3d952c2f471b44fa370bfd5e72c4a8d3
Ítalo Lima <italojonas@hotmail.com>
2021-07-04 12:00:38 -0300

    adjuste docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test"
-        ? host
-        : "database_ignite",
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
